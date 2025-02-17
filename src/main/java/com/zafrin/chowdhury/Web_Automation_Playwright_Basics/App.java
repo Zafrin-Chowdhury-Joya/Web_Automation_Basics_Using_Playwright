55445fcf60da744244eaf8cc448357cdf1572f5e
Zafrin Chowdhury <zafrinchowdhury@Zafrins-MacBook-Air.local>
2025-02-17 16:59:15 +0600

    Initial commit

---
+++ src/main/java/com/zafrin/chowdhury/Web_Automation_Playwright_Basics/App.java
@@ -0,0 +1,13 @@
+package com.zafrin.chowdhury.Web_Automation_Playwright_Basics;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
